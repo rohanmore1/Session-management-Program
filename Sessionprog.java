48adea38f9c857ec79275ad7372677ce9b5f9977
rohanmore1 <69668795+rohanmore1@users.noreply.github.com>
2021-05-25 18:11:56 +0530

    Add files via upload

---
+++ Sessionprog.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;  
+  
+  
+public class Sessionprog extends HttpServlet {  
+  
+  public void doPost(HttpServletRequest request, HttpServletResponse response){  
+    try{  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+          
+    String n=request.getParameter("userName");  
+    out.print("Welcome "+n);  
+  
+    Cookie ck=new Cookie("uname",n);//creating cookie object  
+    response.addCookie(ck);//adding cookie in the response  
+  
+    //creating submit button  
+    out.print("<form action='servlet2'>");  
+    out.print("<input type='submit' value='go'>");  
+    out.print("</form>");  
+          
+    out.close();  
+  
+        }catch(Exception e){System.out.println(e);}  
+  }  
+}  
